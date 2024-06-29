7702b9b4d0c803dec360c3efe67aef7449d78b9a
Sahil <issahil404@gmail.com>
2024-06-29 10:35:48 +0530

    added a helper to read id params

---
+++ cmd/api/helpers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
--- cmd/api/texts.go
+++ cmd/api/texts.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createTextHandler will be used to create a text
@@ -15,12 +12,8 @@ func (app *application) createTextHandler(w http.ResponseWriter, r *http.Request
 
 // showTextHandler will be used to show a text
 func (app *application) showTextHandler(w http.ResponseWriter, r *http.Request) {
-	// when httprouter is used, the parameters are available in the context of the request
-	params := httprouter.ParamsFromContext(r.Context())
-	// convert the id parameter to an integer
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	// if there is an error or id is less than 1, return a 404 Not Found response
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
