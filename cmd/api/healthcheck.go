36fa5e960acb2270dc465f199d16513900a8e51d
Sahil <issahil404@gmail.com>
2024-07-03 04:10:47 +0530

    added writeJSON helper and used it in healthcheck endpoint

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // healthCheckHandler will be used to check the health of the application
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "env": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
 
-	// Only setting content type and not charset because its not required and it is recommended to not set charset as it'll be redundant
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	data := map[string]string{
+		"status":  "available",
+		"config":  app.config.env,
+		"version": version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -15,4 +16,24 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	// Encode the data to JSON, returning the error if there was one
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// append a newline character to the JSON response just to make it easier to read in the terminal
+	js = append(js, '\n')
+
+	// Now, we can safely set the headers as there will be no errors after this point
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
