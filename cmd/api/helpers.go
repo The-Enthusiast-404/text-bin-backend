ad76a96f21e56863dcdeff212eed72edc1890a27
Sahil <issahil404@gmail.com>
2024-07-05 12:20:37 +0530

    updated readJSON() to check for large requests and invalid fields

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -43,7 +44,14 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	maxByes := 1_048_576
+	// Limiting the size of the request body to 1MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxByes))
+	// initializing a new JSON decoder
+	dec := json.NewDecoder(r.Body)
+	// DisallowUnknownFields() method will cause the decoder to return an error if the destination type has a field that can't be set from the JSON
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
 	if err != nil {
 		// if there's an error in decoding, start the triage process
 		var syntaxError *json.SyntaxError
@@ -63,12 +71,25 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown field %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxByes)
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 		default:
 			return err
 		}
 
+	}
+	// Call Decode() again, using a pointer to an empty anonymous struct as the
+	// destination. If the request body only contained a single JSON value this will
+	// return an io.EOF error. So if we get anything else, we know that there is
+	// additional data in the request body and we return our own custom error message.
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }
