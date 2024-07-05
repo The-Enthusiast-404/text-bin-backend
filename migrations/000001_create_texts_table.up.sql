225031418febc97a4828a67ce92efdea1df649e5
Sahil <issahil404@gmail.com>
2024-07-05 13:32:48 +0530

    added migration and finished setting up text table

---
+++ migrations/000001_create_texts_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS texts (
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT now(),
+    title text NOT NULL,
+    content text NOT NULL,
+    format text NOT NULL DEFAULT 'plaintext',
+    version integer NOT NULL DEFAULT 1
+);
